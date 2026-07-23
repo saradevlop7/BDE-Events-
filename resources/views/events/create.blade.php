980f867124d07d7a900a19ec435dda1191596161
sara charafi <elkbircharafi8@gmail.com>
2026-07-23 16:59:52 +0100

    Improve dashboard and login UI design

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -5,6 +5,7 @@
     <title>Créer un événement</title>
 <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    
     <title>Dashboard</title>
 
 </head>
