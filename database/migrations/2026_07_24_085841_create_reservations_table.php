ff23eb6de2d54919f2c0e784fe990bf95525039c
sara charafi <elkbircharafi8@gmail.com>
2026-07-24 10:16:49 +0100

    Implement role-based authentication and admin middleware

--- database/migrations/2026_07_24_085841_create_reservations_table.php
+++ database/migrations/2026_07_24_085841_create_reservations_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        
         Schema::create('reservations', function (Blueprint $table) {
 
             $table->id();
