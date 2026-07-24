b0a6620556017dac7209552023e85ffca78931fa
sara charafi <elkbircharafi8@gmail.com>
2026-07-24 10:17:40 +0100

    Prepare event reservation and ticket management system

--- database/migrations/2026_07_23_093623_create_events_table.php
+++ database/migrations/2026_07_23_093623_create_events_table.php
@@ -27,9 +27,10 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    
+
     public function down(): void
     {
+        
         Schema::dropIfExists('events');
     }
 };
