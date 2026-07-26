a4015577900a816cc7b1ba4d603d54723a41e2ad
sara charafi <elkbircharafi8@gmail.com>
2026-07-26 01:31:29 +0100

    Développer le système de réservation des événements

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -10,6 +10,7 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
+    
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
