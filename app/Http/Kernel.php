5a92cc0990e384952d737f1db2949472724b3a91
sara charafi <elkbircharafi8@gmail.com>
2026-07-26 15:52:48 +0100

    Add project README documentation

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,7 +51,7 @@ class Kernel extends HttpKernel
      * Aliases may be used instead of class names to conveniently assign middleware to routes and groups.
      *
      * @var array<string, class-string|string>
-     * 
+     *
      */
     protected $middlewareAliases = [
         'isAdmin' => \App\Http\Middleware\IsAdmin::class,
@@ -67,4 +67,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+    
 }
